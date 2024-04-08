6a7bf0a28c35a7926049848481f89e85d80beab4
floydcal <160587879+floydcal@users.noreply.github.com>
2024-04-08 14:22:47 +0500

    convertor

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const curRates = {
+    USD: 1,
+    PKR: 271.68,
+    GBP: 0.80,
+    EUR: 0.82,
+    INR: 99.41
+};
+let userInput = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter which currency to convert from:",
+        type: "list",
+        choices: ['USD', 'PKR', 'GBP', 'EUR', 'INR']
+    },
+    {
+        name: "to",
+        message: "Enter which currency to convert to:",
+        type: "list",
+        choices: ['USD', 'PKR', 'GBP', 'EUR', 'INR']
+    },
+    {
+        name: "amount",
+        message: "Enter the amount you wish to convert:",
+        type: "number",
+    },
+]);
+let amountFrom = curRates[userInput.from];
+let amountTo = curRates[userInput.to];
+let amount = userInput.amount;
+let baseCurrency = amount / amountFrom;
+let amountCovert = amountTo * baseCurrency;
+console.log(amountCovert);
---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import { userInfo } from "os";
+
+const curRates:any = {
+    USD: 1,
+    PKR: 271.68,
+    GBP: 0.80,
+    EUR: 0.82,
+    INR: 99.41
+};
+
+let userInput = await inquirer.prompt([
+    {
+    name: "from",
+    message: "Enter which currency to convert from:",
+    type: "list",
+    choices: ['USD', 'PKR', 'GBP', 'EUR', 'INR']
+    },
+
+    {
+        name: "to",
+        message: "Enter which currency to convert to:",
+        type: "list",
+        choices: ['USD', 'PKR', 'GBP', 'EUR', 'INR']
+    },
+    {
+        name: "amount",
+        message: "Enter the amount you wish to convert:",
+        type: "number",
+        
+    },
+
+])
+
+let amountFrom= curRates [userInput.from];
+let amountTo= curRates[userInput.to];
+let amount = userInput.amount
+let baseCurrency = amount / amountFrom
+let amountCovert = amountTo * baseCurrency ;
+console.log (amountCovert);
+
+
+
